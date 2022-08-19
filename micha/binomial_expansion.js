d2698fb9037019dfe8cbaf6ce52a896555201c84
micha <michavardy@gmail.com>
2022-08-19 13:10:53 +0300

    tutorial 3

---
+++ micha/binomial_expansion.js
@@ -0,0 +1,11 @@
+let x = 2
+let y = 2
+let n = 2
+let result 
+
+
+result = (x+y)**n
+
+console.log(result)
+
+
---
+++ micha/bitwise.js
@@ -0,0 +1,7 @@
+let num = 150
+
+let num_bin // an operation to find binary rep of num
+let num_bin_plus_one  // shifted
+
+console.log(num_bin)
+console.log(num_bin_plus_one)
