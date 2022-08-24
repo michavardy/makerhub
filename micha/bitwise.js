24c386e5c27cd94af8a9ee489320a8dc4aaa828f
micha <michavardy@gmail.com>
2022-08-24 22:46:29 +0300

    bitwise

--- micha/bitwise.js
+++ micha/bitwise.js
@@ -1,7 +1,21 @@
-let num = 150
+let num = 7
 
-let num_bin // an operation to find binary rep of num
-let num_bin_plus_one  // shifted
+function addOne(x) {
+    let m = 1;
+     
+    // Flip all the set bits
+    // until we find a 0
+    while( x & m ) {
+        x = x ^ m;
+        m <<= 1;
+    }
+     
+    // flip the rightmost 0 bit
+    x = x ^ m;
+    return x;
+}
 
+let num_bin = (num).toString(2)
+let num_bin_plus_one  = addOne(num).toString(2)
 console.log(num_bin)
 console.log(num_bin_plus_one)
